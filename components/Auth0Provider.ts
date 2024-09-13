f5f88b2713338a7a089db27ee9f263fa7fd364fe
Randall Miller <randallsm83@gmail.com>
2024-09-13 14:11:28 -0700

    no semis

--- components/Auth0Provider.ts
+++ components/Auth0Provider.ts
@@ -1,9 +1,9 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react';
-import App from './App';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react'
+import App from './App'
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'))
 
 root.render(
 <Auth0Provider
@@ -15,4 +15,4 @@ root.render(
   >
     <App />
   </Auth0Provider>,
-);
+)
