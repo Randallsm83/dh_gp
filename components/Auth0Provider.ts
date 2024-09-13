a6b9e2b48317624b66becb7a82a4783e09bd2981
Randall Miller <randallsm83@gmail.com>
2024-09-13 14:10:24 -0700

    Start of our app with login components setup

---
+++ components/Auth0Provider.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+<Auth0Provider
+    domain="http://dev-md802pukuf77blph.us.auth0.com/"
+    clientId="NqTKdYVfNpUWUMa8GwVg3q1ltcdopKPA"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <App />
+  </Auth0Provider>,
+);
