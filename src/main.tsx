1711246d9e967acdf69ddede97026efe2167d55c
Randall Miller <randallsm83@gmail.com>
2024-09-20 17:03:02 -0700

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Auth0Provider
+      domain="http://dev-md802pukuf77blph.us.auth0.com"
+      clientId="NqTKdYVfNpUWUMa8GwVg3q1ltcdopKPA"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
 )
