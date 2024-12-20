744af250e35d08f53627204329cc177a499e72ba
raul <gilbertoallovera@gmail.com>
2024-12-20 17:01:33 -0500

    server and client apps

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from "./routes/root";
+import axios from "axios";
+
 
 const router = createBrowserRouter([
   {
@@ -9,10 +11,18 @@ const router = createBrowserRouter([
   }
 ])
 
+const apiCall = () => {
+  axios.get('http://localhost:8080').then((data) => {
+    //this console.log will be in our frontend console
+    console.log(data)
+  })
+}
+
 function App() {
   return (
     <>
       <div className="card">
+	<button onClick={apiCall}>Make API Call</button>
         <RouterProvider router={router} />
       </div>
     </>
